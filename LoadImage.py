482ce0470223187209869f6798f9c0de0fbf8e4b
bowenzhao <bzhao19@ucsc.edu>
2023-09-29 19:41:32 -0400

    prototype of count images in each folder

--- LoadImage.py
+++ LoadImage.py
@@ -1,11 +1,12 @@
 import requests
 import os
 import time
+import matplotlib.pyplot as plt
 
 # Github Repo Info
 repoOwner = 'yanglinfang'
 repoName = 'npo_tech_wildlife_camera'
-folderPath = 'testimage'
+folderPath = 'dataset'
 
 # GitHub API endpoint to list contents of the folder
 apiUrl = f'https://api.github.com/repos/{repoOwner}/{repoName}/contents/{folderPath}'
@@ -13,55 +14,53 @@ apiUrl = f'https://api.github.com/repos/{repoOwner}/{repoName}/contents/{folderP
 # Retry times when failed to download image
 maxRetry = 10
 
-# Send HTTP GET request
-response = requests.get(apiUrl)
+# Dictionary to store image count inside each folder
+folderCounts = {}
 
-#Check if request was successful(status code 200)
-if response.status_code == 200:
-    # print('Successfully get folder')
+def load_image_from_folder(apiUrl, maxRetry = 10):
+    # Send HTTP GET request
+    response = requests.get(apiUrl)
 
-    # Turn the folder into a file 
-    folderContents = response.json()
+    #Check if request was successful(status code 200)
+    if response.status_code == 200:
 
-    # Create a dir to save downloaded images
-    if not os.path.exists(folderPath):
-        os.makedirs(folderPath)
+        # Turn the folder into a file 
+        folderContents = response.json()
 
-    # Loop through contents of folder
-    for item in folderContents:
-        if item['type'] == 'file':
-            fileName = item['name']
-            downloadUrl = item['download_url']
+        # Loop through contents of folder
+        for item in folderContents:
+            if item['type'] == 'dir':
+                subFolderUrl = item['url']
+                # folderName = item['name']
+                # folderCounts[folderName] = 0
+                load_image_from_folder(subFolderUrl, maxRetry)
+            elif item['type'] == 'file':
+                folderName = os.path.dirname(item['path'])
+                folderCounts[folderName] = folderCounts.get(folderName, 0) + 1
 
-            for retry in range(maxRetry):
-                try:
-                    # Send an HTTP GET request to download the image
-                    imageResponse = requests.get(downloadUrl, timeout=10)
+                # fileName = item['name']
+                # downloadUrl = item['download_url']
 
-                    if imageResponse.status_code == 200:
-                        print(f'Load image {fileName} successfull')
-                        break
+                # for retry in range(maxRetry):
+                #     try:
+                #         # Send an HTTP GET request to download the image
+                #         imageResponse = requests.get(downloadUrl, timeout=10)
 
-                except requests.exceptions.RequestException as e:
-                    time.sleep(1)
-            else:
-                print(f'Failed to downloadimage {fileName} after {maxRetry} retries')
-            
-            """ this code is for if want to download images to local"""
-            # for retry in range(maxRetry):
-            #     try:
-            #         # Send an HTTP GET request to download the image
-            #         imageResponse = requests.get(downloadUrl, timeout=10)
+                #         if imageResponse.status_code == 200:
+                #             # readImage = imageResponse.content
+                #             print(f'Load image {fileName} successfull')
+                #             break
 
-            #         if imageResponse.status_code == 200:
-            #             with open(os.path.join(folderPath, fileName), 'wb') as f:
-            #                 f.write(imageResponse.content)
-            #             print(f'Successfully downloaded: {fileName}')
-            #             break
-            #     except (requests.ConnectionError, requests.Timeout):
-            #         print(f'Retry #{retry + 1} failed to fetch image {fileName}. Retrying...')
-            #         time.sleep(1)  # Wait for a moment before retrying
-            # else:
-            #     print(f'Failed to downloadimage {fileName} after {maxRetry} retries')
-else:
-    print(f'Failed to fetch folder: Status Code {response.status_code}')
+                #     except requests.exceptions.RequestException as e:
+                #         time.sleep(1)
+                # else:
+                #     print(f'Failed to downloadimage {fileName} after {maxRetry} retries')
+    else:
+        print(f'Failed to fetch folder: Status Code {response.status_code}')
+
+load_image_from_folder(apiUrl, maxRetry)
+print(folderCounts)
+
+folderName = list(folderCounts.keys())
+imageCount = list(folderCounts.values())
+
